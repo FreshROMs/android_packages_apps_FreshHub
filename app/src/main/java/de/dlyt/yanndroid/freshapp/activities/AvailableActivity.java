58ddebe46119141132918c546e64b041adac8a2f
John Vincent <corcegajovic@gmail.com>
2021-03-26 16:10:33 +0800

    Add LinkMovementMethod to AvailableActivity

--- app/src/main/java/de/dlyt/yanndroid/freshapp/activities/AvailableActivity.java
+++ app/src/main/java/de/dlyt/yanndroid/freshapp/activities/AvailableActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
