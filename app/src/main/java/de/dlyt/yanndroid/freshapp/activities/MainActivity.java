c9cfc47b34b20c6e20ffb091e8e82c438e45d0ea
John Vincent <corcegajovic@gmail.com>
2021-04-15 18:17:48 +0800

    Finished Community card

--- app/src/main/java/de/dlyt/yanndroid/freshapp/activities/MainActivity.java
+++ app/src/main/java/de/dlyt/yanndroid/freshapp/activities/MainActivity.java
@@ -101,7 +101,7 @@ public class MainActivity extends AppCompatActivity implements Constants,
 
     private boolean updateAllLayouts() {
         try {
-            // updateDonateLinkLayout();
+            updateCommunityLinksLayout();
             updateAddonsLayout();
             updateRomInformation();
             updateRomUpdateLayouts(true);
@@ -176,7 +176,7 @@ public class MainActivity extends AppCompatActivity implements Constants,
         Utils.setHasFileDownloaded(mContext);
 
         // Update the layouts
-        //updateDonateLinkLayout();
+        updateCommunityLinksLayout();
         updateAddonsLayout();
         updateRomInformation();
         updateRomUpdateLayouts(false);
@@ -661,22 +661,74 @@ public class MainActivity extends AppCompatActivity implements Constants,
         }
     }
 
-    private void updateDonateLinkLayout() {
-        MaterialCardView donateLink = (MaterialCardView) findViewById(R.id.layout_main_dev_donate_link);
-        donateLink.setVisibility(View.GONE);
+    private void updateCommunityLinksLayout() {
+        LinearLayout linkXdaForum = (LinearLayout) findViewById(R.id.layout_main_forum);
+        LinearLayout linkDiscord = (LinearLayout) findViewById(R.id.layout_main_discord);
+        LinearLayout linkTelegram = (LinearLayout) findViewById(R.id.layout_main_telegram);
+        LinearLayout linkDiscussions = (LinearLayout) findViewById(R.id.layout_main_discussions);
+        LinearLayout linkWebsite = (LinearLayout) findViewById(R.id.layout_main_website);
+        LinearLayout linkDonate = (LinearLayout) findViewById(R.id.layout_main_donate);
+
+        linkXdaForum.setVisibility(View.GONE);
+        linkDiscord.setVisibility(View.GONE);
+        linkTelegram.setVisibility(View.GONE);
+        linkDiscussions.setVisibility(View.GONE);
+        linkWebsite.setVisibility(View.GONE);
+        linkDonate.setVisibility(View.GONE);
+
+        if (!RomUpdate.getForum(mContext).trim().equals("null")) {
+            linkXdaForum.setVisibility(View.VISIBLE);
+            linkXdaForum.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(RomUpdate.getForum(mContext))));
+                }
+            });
+        }
+
+        if (!RomUpdate.getDiscord(mContext).trim().equals("null")) {
+            linkDiscord.setVisibility(View.VISIBLE);
+            linkDiscord.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(RomUpdate.getDiscord(mContext))));
+                }
+            });
+        }
+
+        if (!RomUpdate.getGitIssues(mContext).trim().equals("null")) {
+            linkTelegram.setVisibility(View.VISIBLE);
+            linkTelegram.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(RomUpdate.getDiscord(mContext))));
+                }
+            });
+        }
+
+        if (!RomUpdate.getGitDiscussion(mContext).trim().equals("null")) {
+            linkDiscussions.setVisibility(View.VISIBLE);
+            linkDiscussions.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(RomUpdate.getGitDiscussion(mContext))));
+                }
+            });
+        }
+
+        if (!RomUpdate.getWebsite(mContext).trim().equals("null")) {
+            linkWebsite.setVisibility(View.VISIBLE);
+            linkWebsite.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(RomUpdate.getWebsite(mContext))));
+                }
+            });
+        }
 
         if (!(RomUpdate.getDonateLink(mContext).trim().equals("null"))
                 || !(RomUpdate.getBitCoinLink(mContext).trim().equals("null"))) {
-            donateLink.setVisibility(View.VISIBLE);
-        }
-    }
-
-    private void updateWebsiteLayout() {
-        MaterialCardView webLink = (MaterialCardView) findViewById(R.id.layout_main_dev_website);
-        webLink.setVisibility(View.GONE);
-
-        if (!RomUpdate.getWebsite(mContext).trim().equals("null")) {
-            webLink.setVisibility(View.VISIBLE);
+            linkDonate.setVisibility(View.VISIBLE);
         }
     }
 
@@ -779,9 +831,46 @@ public class MainActivity extends AppCompatActivity implements Constants,
         startActivity(intent);
     }
 
-    public void openSettings(View v) {
-        Intent intent = new Intent(mContext, SettingsActivity.class);
-        startActivityForResult(intent, CHANGE_THEME_REQUEST_CODE);
+    public void openForum(View v) {
+        String url = RomUpdate.getForum(mContext);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+
+    public void openDiscord(View v) {
+        String url = RomUpdate.getDiscord(mContext);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+
+    public void openGitDiscussions(View v) {
+        String url = RomUpdate.getGitDiscussion(mContext);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+
+    public void openTelegram(View v) {
+        String url = "https://t.me/FreshROMsCommunity";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    // There are no request codes
+                    Intent data = result.getData();
+                }
+            });
+
+    public void openAbout(View v) {
+        Intent intent = new Intent(mContext, AboutActivity.class);
+        someActivityResultLauncher.launch(intent);
     }
 
 
