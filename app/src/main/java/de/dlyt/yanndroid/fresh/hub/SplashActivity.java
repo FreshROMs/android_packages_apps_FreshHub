450874c44d674d2dcf80a0fda07f6ea842305823
Yanndroid <57589186+Yanndroid@users.noreply.github.com>
2021-04-24 13:37:35 +0200

    - splash redesign

--- app/src/main/java/de/dlyt/yanndroid/fresh/hub/SplashActivity.java
+++ app/src/main/java/de/dlyt/yanndroid/fresh/hub/SplashActivity.java
@@ -1,8 +1,13 @@
 package de.dlyt.yanndroid.fresh.hub;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,11 +20,42 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        ImageView fresh_fg = findViewById(R.id.splash_image_foreground);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent().setClass(getApplicationContext(), MainActivity.class));
-                finish();
+
+                Animation splash_anim;
+                splash_anim = AnimationUtils.loadAnimation(getBaseContext(), R.anim.splash_animation);
+
+                splash_anim.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        startActivity(new Intent().setClass(getApplicationContext(), MainActivity.class));
+                        finish();
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+
+                fresh_fg.startAnimation(splash_anim);
+
+                /*fresh_fg.animate().setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        startActivity(new Intent().setClass(getApplicationContext(), MainActivity.class));
+                        finish();
+                    }
+                }).rotation(360).setDuration(1000).start();*/
             }
         }, getResources().getInteger(R.integer.splash_time));
 
