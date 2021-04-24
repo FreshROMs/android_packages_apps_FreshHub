e0c9f3213d2c70222b46fe3e9cb4d1c181af38a5
Yanndroid <57589186+Yanndroid@users.noreply.github.com>
2021-04-24 13:47:52 +0200

    - splash redesign

--- app/src/main/java/de/dlyt/yanndroid/fresh/hub/SplashActivity.java
+++ app/src/main/java/de/dlyt/yanndroid/fresh/hub/SplashActivity.java
@@ -1,7 +1,5 @@
 package de.dlyt.yanndroid.fresh.hub;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -22,40 +20,31 @@ public class SplashActivity extends AppCompatActivity {
 
         ImageView fresh_fg = findViewById(R.id.splash_image_foreground);
 
+        Animation splash_anim;
+        splash_anim = AnimationUtils.loadAnimation(getBaseContext(), R.anim.splash_animation);
+
+        splash_anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent().setClass(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
-                Animation splash_anim;
-                splash_anim = AnimationUtils.loadAnimation(getBaseContext(), R.anim.splash_animation);
-
-                splash_anim.setAnimationListener(new Animation.AnimationListener() {
-                    @Override
-                    public void onAnimationStart(Animation animation) {
-
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animation animation) {
-                        startActivity(new Intent().setClass(getApplicationContext(), MainActivity.class));
-                        finish();
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animation animation) {
-
-                    }
-                });
-
                 fresh_fg.startAnimation(splash_anim);
-
-                /*fresh_fg.animate().setListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        startActivity(new Intent().setClass(getApplicationContext(), MainActivity.class));
-                        finish();
-                    }
-                }).rotation(360).setDuration(1000).start();*/
             }
         }, getResources().getInteger(R.integer.splash_time));
 
